be4c9e469684bad4a0638cbe73e9176b09745290
Douglas Pinheiro <dougcpinheiro@yahoo.com>
2019-10-04 21:23:20 +0100

    [AIRCNC - 003] - Database Configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-fakef.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 /*Middlewares*/
 app.use(express.json());
