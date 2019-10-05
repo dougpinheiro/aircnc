ea0bc93e4d75f28147b011a0af35b94364863e80
Douglas Pinheiro <dougcpinheiro@yahoo.com>
2019-10-06 00:52:04 +0100

    [AIRCNC] - Primeiro commit do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -10,7 +12,10 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-fakef.mongodb.net
 });
 
 /*Middlewares*/
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
+/**/
 
 app.listen(3333);
